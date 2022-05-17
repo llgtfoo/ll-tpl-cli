65d10167f469c0d17166308369ee6be263db66a5
前端小丑 <lltfoo@163.com>
2022-05-17 21:44:21 +0800

    优化

--- vue/list.js
+++ vue/list.js
@@ -6,29 +6,29 @@
  * @LastEditors: llgtfoo
  * @FilePath: \ll-tpl-cli-pro\vue\list.js
  */
-const Table = require('cli-table2')//表格输出
-const chalk = require('chalk')//字体改变
-const templateList = require('../templates.json')//模板json
-const { log } = require('../plugins/log.js')//日志输出
+const Table = require('cli-table2') //表格输出
+const chalk = require('chalk') //字体改变
+const templateList = require('../templates.json') //模板json
+const { log } = require('../plugins/log.js') //日志输出
 module.exports = function list() {
     console.log('Available official templates:')
     console.log()
-    // Object.keys(templateList).forEach(tpl => {
-    //   console.log(
-    //     `  ${chalk.yellow('★')
-    //     }  ${chalk.blue.bold(templateList[tpl].name)
-    //     } + ${templateList[tpl].version
-    //     } + ${templateList[tpl].type
-    //     } + ${templateList[tpl].description}`)
-    // })
+        // Object.keys(templateList).forEach(tpl => {
+        //   console.log(
+        //     `  ${chalk.yellow('★')
+        //     }  ${chalk.blue.bold(templateList[tpl].name)
+        //     } + ${templateList[tpl].version
+        //     } + ${templateList[tpl].type
+        //     } + ${templateList[tpl].description}`)
+        // })
     const table = new Table({
-        head: ['名称', '版本', '类型', '描述'],
-    })
-    // eslint-disable-next-line guard-for-in
+            head: ['名称', '版本', '类型', '描述'],
+        })
+        // eslint-disable-next-line guard-for-in
     for (const key in templateList) {
         table.push(
             [
-                `${chalk.yellow.bold('*')} ${chalk.blue.bold(templateList[key].name)}`,
+                `${chalk.yellow.bold(templateList[key].index)}、${chalk.blue.bold(templateList[key].name)} `,
                 templateList[key].version,
                 templateList[key].type,
                 templateList[key].description,
@@ -37,6 +37,6 @@ module.exports = function list() {
     }
     console.log(table.toString())
     console.log()
-    console.log(`ll-tpl-cli地址：${log.info('https://github.com/llgtfoo/ll-tpl-cli.git')}`)
+        // console.log(`ll - tpl - cli地址：${ log.info('https://github.com/llgtfoo/ll-tpl-cli.git') } `)
     console.log()
 }
